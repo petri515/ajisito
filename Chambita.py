3057e24253c84c548639a6a6b53c5773b611fa5a
Juaraquer <juaraquer@unal.edu.co>
2025-04-01 15:43:16 -0500

    Add files via upload

---
+++ Chambita.py
@@ -0,0 +1,36 @@
+# PRIMER EJERCICIO
+
+print("Hola, ya se imprimir frases")
+
+#Segundo 
+
+print(420)
+
+#tercero
+print(5.3)
+
+#1.2.Operaciones básicas y bucles               
+
+# cuarto
+suma1,suma2 =1234,532 
+print(f"{suma1} + {suma2} = {suma1+suma2} ")
+
+# Quinto
+resta1,resta2 =1234,532
+print(resta1,"+",resta2,"=", resta1-resta2 )
+
+#Sexto 
+
+print(1234*532)
+
+#septimo
+
+print(1234/532)
+
+#octavo
+sucesion_1_3 = 0
+while sucesion_1_3 < 3:
+    
+    sucesion_1_3 += 1
+
+    print(sucesion_1_3)

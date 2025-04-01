217e473bc55c62f7ed1a8159db2185e84741802b
Juaraquer <juaraquer@unal.edu.co>
2025-04-01 17:50:38 -0500

    Tengo fé

--- Chambita.py
+++ Chambita.py
@@ -2,11 +2,11 @@
 
 print("Hola, ya se imprimir frases")
 
-#Segundo 
+#   Segundo 
 
 print(420)
 
-#tercero
+#   tercero
 print(5.3)
 
 #1.2.Operaciones básicas y bucles               
@@ -19,18 +19,73 @@ print(f"{suma1} + {suma2} = {suma1+suma2} ")
 resta1,resta2 =1234,532
 print(resta1,"+",resta2,"=", resta1-resta2 )
 
-#Sexto 
+#   Sexto 
 
 print(1234*532)
 
-#septimo
+#   Septimo
 
 print(1234/532)
 
-#octavo
+#   Octavo
 sucesion_1_3 = 0
 while sucesion_1_3 < 3:
-    
+
     sucesion_1_3 += 1
 
     print(sucesion_1_3)
+
+#   Noveno 
+for i in range(10):
+    print(i)
+
+#   Decimo
+"""
+numero_1_10k =list(range(1,10001))
+
+print(", ".join(map(str,numero_1_10k)))
+"""
+#   Undecimo
+for i in range(5,11):
+
+    print(i)
+
+#   Duodecimo
+
+for i in range(5,16):
+
+    print(i,end="|",)
+    """
+#   Decimotercero
+sucesion_5_15K = 0
+while sucesion_5_15K < 15000:
+    sucesion_5_15K += 1
+    print(sucesion_5_15K, end=" ")
+"""
+print("\n")
+#   Decimocuarto
+
+holiwis = "hola"
+
+for i in range(200):
+
+    print(holiwis,end=" ")
+
+print("\n")
+#   Decimoquinto
+
+for i in range(1,31):
+    print(i**2)
+
+#   Decimosexto
+Y = 1
+for i in range(1,21):
+     Y = Y*i
+print(Y)
+
+#   Decimoseptimo
+X = 0
+for i in range(1,101):
+
+    X += i**2
+print(X)    
